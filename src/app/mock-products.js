b512685c93d12cbf12e14794c4c68f0a69e37cc8
rafaelmum <rcosta@mum.edu>
2017-10-16 23:07:01 -0500

    Lesson W3D1 - Added Product Service, Promise and Observable.

---
+++ src/app/mock-products.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.PRODUCTS = [
+    { id: 11, name: 'Sandals', price: 5.00 },
+    { id: 12, name: 'Ball', price: 3.00 },
+    { id: 13, name: 'Shirt', price: 15.00 },
+    { id: 14, name: 'Shoes', price: 40.00 },
+    { id: 15, name: 'Mouse', price: 3.00 },
+    { id: 16, name: 'Keyboard', price: 5.00 },
+    { id: 17, name: 'Laptop', price: 300.00 },
+    { id: 18, name: 'TV', price: 400.00 },
+    { id: 19, name: 'Car', price: 15000.00 },
+    { id: 20, name: 'Coffee Machine', price: 8.00 }
+];
+//# sourceMappingURL=mock-products.js.map
---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from './product';
+import { PRODUCTS } from './mock-products';
+
+@Injectable()
+export class ProductService {
+
+    // USING SYNCHRONOUSLY/WITH OBSERVABLE.
+    getProducts(): Product[] {
+        return PRODUCTS;
+    }
+    
+    // USING PROMISE.
+    //getProducts(): Promise<Product[]> {
+        //return Promise.resolve(PRODUCTS);
+    //}
+}
